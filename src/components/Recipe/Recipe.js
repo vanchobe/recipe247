03aeff1d6fb7fd5531959175fdd99a22fc5d31ce
vanchobe <ivandm555@gmail.com>
2021-03-14 16:48:43 +0200

    add pagination and grid system in recipes component

--- src/components/Recipe/Recipe.js
+++ src/components/Recipe/Recipe.js
@@ -4,15 +4,30 @@ import {
     Link
   } from "react-router-dom";
 
-const Recipe = ({name, image, prepareTime, portions, description, _id}) => {
+import {Card, Button, Row, Text} from 'react-bootstrap';  
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faClock, faFileSignature, faGripHorizontal  } from '@fortawesome/free-solid-svg-icons'
+
+const portionsIcon = <FontAwesomeIcon icon={faGripHorizontal} />
+const prepareTimeIcon = <FontAwesomeIcon icon={faClock} />
+const howToCookIcon = <FontAwesomeIcon icon={faFileSignature} />
+
+const Recipe = ({name, image, prepareTime, portions, description, _id, index}) => {
     return (
-        <div>
-            <p><Link to={`/preview-recipe/${_id}`}>  Recipe name : {name}</Link></p>
-            <p><img src={image}/></p>
-            <p>Prepare Time: {prepareTime}</p>
-            <p>Portions: {portions}</p>
-            <p>How to: {description}</p>     
-        </div>
+        <Row>
+        <Card className='mt-2 mr-2 mb-2' style={{ width: '25rem' }}>
+        <Card.Img variant="top" src={image} />
+        <Card.Body>
+          <Card.Title>{name}</Card.Title>
+          
+          <Card.Text>{portionsIcon} {portions} порции</Card.Text>
+          <Card.Text>{prepareTimeIcon} {prepareTime} минути</Card.Text>
+          <Card.Text>{howToCookIcon} {description.length > 10 ? description.substring(0,10) + '...' : description}</Card.Text>
+           
+          <Button as={Link} to={`/preview-recipe/${_id}`} variant="primary">View Recipe</Button>
+        </Card.Body>
+      </Card>
+      </Row>
     )
 }
 export default Recipe
--- src/components/Recipes/Recipes.js
+++ src/components/Recipes/Recipes.js
@@ -3,54 +3,104 @@ import { auth } from "../../services/firebase";
 import { db } from "../../services/firebase"
 import { useState, useEffect } from 'react';
 import  Recipe  from '../Recipe/Recipe';
+import ReactPaginate from 'react-paginate';
+import { Row, Col } from 'react-bootstrap'; 
 
 const Recipes = props => {
     const [user, setUser] = useState(auth().currentUser);
     const [recipes, setRecipes] = useState([]);
     const [readError, setReadError] = useState(null);
+    const [currentPage, setCurrentPage] = useState(0);
 
+
+    const loadRecipes = (isSubscribed) => {
+      try {
+        db.ref("recipes").on("value", snapshot => {
+          let recipes = [];
+          snapshot.forEach((snap) => {
+          
+            let _id = snap.ref_.path.pieces_[1];
+            recipes.push({...snap.val(), _id});
+            
+          });
+          if(isSubscribed){
+          setRecipes( recipes.reverse() );
+          }
+          
+        });
+      } catch (error) {
+        if(isSubscribed){
+        setReadError(error.message);
+        }
+      }
+    }
 
       useEffect(() => {
         let isSubscribed = true;
         if(isSubscribed){
         setReadError(null)
         }
-        try {
-          db.ref("recipes").on("value", snapshot => {
-            let recipes = [];
-            snapshot.forEach((snap) => {
-            
-              let _id = snap.ref_.path.pieces_[1];
-              recipes.push({...snap.val(), _id});
-              
-            });
-            if(isSubscribed){
-            setRecipes( recipes.reverse() );
-            }
-            
-          });
-        } catch (error) {
-          if(isSubscribed){
-          setReadError(error.message);
-          }
-        }
+        loadRecipes(isSubscribed);
         return () => (isSubscribed = false)
       }, []);
 
+      const PER_PAGE = 5;
+      const COLS_PER_ROW = 3;
+      const offset = currentPage * PER_PAGE;
+      let pagingRecipes = [...recipes];
+      const currentPageRecipes = pagingRecipes
+          .slice(offset, offset + PER_PAGE)
+          .reduce(
+              (rows, key, index) =>
+                (index % COLS_PER_ROW === 0
+                  ? rows.push([key])
+                  : rows[rows.length - 1].push(key)) && rows,
+              []
+            )
+            .map((items, index) =>  (
+              <Row key={index}>
+                {items.map(item =>  (
+                  
+                  <Col  key={item._id} lg={3}>
+                  <Recipe
+                       key={item._id}
+                      _id={item._id}
+                      name={item.name}
+                      image= {item.image}
+                      prepareTime = {item.prepareTime}
+                      portions = {item.portions}
+                      description = {item.description}
+                      />
+                  </Col>
+                ))}
+              </Row>
+            ))
+          
+         
+           
+     
+        
+           ;
+      const pageCount = Math.ceil(recipes.length / PER_PAGE);
+      function handlePageClick({ selected: selectedPage }) {
+        setCurrentPage(selectedPage);
+    }
+    
     return (
         <div>
            <div className={style.recipesContainer}>
-        {recipes.map((recipe, index) => {
-          return <Recipe
-          key={index}
-          _id={recipe._id}
-          name={recipe.name}
-          image= {recipe.image}
-          prepareTime = {recipe.prepareTime}
-          portions = {recipe.portions}
-          description = {recipe.description}
-          />
-        })}
+           <ReactPaginate
+        previousLabel={"← Previous"}
+        nextLabel={"Next →"}
+        pageCount={pageCount}
+        onPageChange={handlePageClick}
+        containerClassName={"pagination"}
+        previousLinkClassName={"pagination__link"}
+        nextLinkClassName={"pagination__link"}
+        disabledClassName={"pagination__link--disabled"}
+        activeClassName={"pagination__link--active"}
+      />
+        {currentPageRecipes}
       </div>
       <div>
         Login in as: <strong className={style.loggedWith}>{user.email}</strong>
