dfe1cb4c4e5ca669b85aba39f7c1bcd99b71cda2
vanchobe <ivandm555@gmail.com>
2021-03-16 11:37:02 +0200

    add styles for addRecipe component and little styles for recipe comp

--- src/components/AddRecipe/AddRecipe.js
+++ src/components/AddRecipe/AddRecipe.js
@@ -4,6 +4,19 @@ import { auth, db } from '../../services/firebase'
 
 import { useHistory } from 'react-router-dom';
 
+import styles from './AddRecipe.module.css';
+
+import { Container, Form, Button } from 'react-bootstrap';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faImage, faGripHorizontal, faClock, faFileSignature, faBreadSlice } from '@fortawesome/free-solid-svg-icons'
+
+const nameIcon = <FontAwesomeIcon icon={faBreadSlice} />;
+const imageIcon = <FontAwesomeIcon icon={faImage} />;
+const portionsIcon = <FontAwesomeIcon icon={faGripHorizontal} />;
+const prepareTimeIcon = <FontAwesomeIcon icon={faClock} />;
+const howToCookIcon = <FontAwesomeIcon icon={faFileSignature} />;
+
 const AddRecipe = ({ logout, authenticated }) => {
   
   const [user, setUser] = useState(auth().currentUser);
@@ -52,31 +65,43 @@ const AddRecipe = ({ logout, authenticated }) => {
 }
 
     return (
-        <div>
-     
-        <form onSubmit={submitValue}>
-           <div>
-            <input placeholder="Name" name="name" type="text" onChange={e => setName(e.target.value)} value={name}></input>
-          </div>
-          <div>
-            <input placeholder="Image" name="image" type="text" onChange={e => setImage(e.target.value)} value={image}></input>
-          </div>
-          <div>
-            <input placeholder="Prepare time" name="prepareTime" type="number" onChange={e => setPrepareTime(e.target.value)} value={prepareTime}></input>
-          </div>
-          <div>
-            <input placeholder="Portions" name="portions" type="number" onChange={e => setPortions(e.target.value)} value={portions}></input>
-          </div>
-          <div>
-            <input placeholder="Description" name="description" type="text" onChange={e => setDescription(e.target.value)} value={description}></input>
-          </div>
-          {writeError ? <p>{writeError}</p> : null}
-          <button type="submit">Send</button>
-        </form>
-        <div>
-          Login in as: <strong>{user.email}</strong>
-        </div>
-      </div>
+      <Container className='col-md-6 mt-3'>
+      <Form onSubmit={submitValue} className={styles.bdForm}>
+        <Form.Group controlId="name">
+        <Form.Label>{nameIcon} Име</Form.Label>
+           <Form.Control placeholder="Въведи име..." name="name" type="text" onChange={e => setName(e.target.value)} value={name} />
+           <Form.Text className="text-muted">
+                 Кратко име на рецептата
+            </Form.Text>
+        </Form.Group>
+        
+        <Form.Group controlId="image">
+        <Form.Label>{imageIcon} Снимка</Form.Label>
+           <Form.Control placeholder="Въведи снимка..." name="image" type="text" onChange={e => setImage(e.target.value)} value={image} />
+        </Form.Group>
+
+        <Form.Group controlId="prepareTime">
+        <Form.Label>{prepareTimeIcon} Време за приготвяне</Form.Label>
+           <Form.Control placeholder="Време за приготвяне..." name="prepareTime" type="number" onChange={e => setPrepareTime(e.target.value)} value={prepareTime} />
+        </Form.Group>
+
+        <Form.Group controlId="portions">
+        <Form.Label>{portionsIcon} Порции</Form.Label>
+           <Form.Control placeholder="Въведи порции..." name="portions" type="number" onChange={e => setPortions(e.target.value)} value={portions} />
+        </Form.Group>
+          
+        <Form.Group controlId="description">
+        <Form.Label>{howToCookIcon} Описание</Form.Label>
+           <Form.Control placeholder="Въведи описание..." name="description" type="text" onChange={e => setDescription(e.target.value)} value={description} />
+        </Form.Group>
+        
+          
+         {writeError ? <p>{writeError}</p> : null}
+         <Button variant="primary" type="submit">
+             Добави рецептата
+          </Button>
+       </Form>
+       </Container>
     )
 }
 
