67cb172725bf285cd2a579f14749fb9bffbcf69c
vanchobe <ivandm555@gmail.com>
2021-03-17 11:27:25 +0200

    add some content to home component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,18 @@
-
+import { Jumbotron, Container, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 const Home = props => {
     return (
-        <div>
-            <h1>
-                This is home component
-            </h1>
-        </div>
+        <Container>
+            <Jumbotron>
+  <h1>Добре дошли в света на рецептите</h1>
+  <p>
+    Станете част от нашата общонст. Започнете още от днес, като добавите вашата първа рецепта.
+  </p>
+  <p>
+    <Button as={Link} to='/add-recipe' variant="primary">Добави рецепта</Button>
+  </p>
+</Jumbotron>
+        </Container>
     )
 }
 
